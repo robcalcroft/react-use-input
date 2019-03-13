c1ff43ee5a3c80eed974d5225148d0adbc5ea5bd
Rob Calcroft <robcalcroft@users.noreply.github.com>
2019-03-13 15:41:56 +0000

    feat: Add code, setup and linting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-use-input
+A hook whose setter can be directly given to HTML inputs
---
+++ index.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function useInput(initialState = '', valueKey = 'value') {
+  const [value, setValue] = useState(initialState);
+
+  function setValueFromEvent(event) {
+    setValue(event.target[valueKey]);
+  }
+
+  return [value, setValueFromEvent];
+}
